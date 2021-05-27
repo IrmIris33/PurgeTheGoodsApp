884ed59075b5e2998d595169cd6c1d6c0990c4bc
IrmIris33 <irmirisortizortiz@outlook.com>
2021-05-27 19:06:07 -0400

    Hackathon

---
+++ purgethegoodsapp/src/Components/login/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import Categories from "../Categories";
+
+function LogIn() {
+
+const routeChange = () =>{
+    let history = useHistory();
+
+    const handleRoute = () =>{
+    history.push("Categories")
+    }
+}
+    return (
+        <div className="component">
+            <p>Username</p>
+            <input type="text" name="login" className="username"></input>
+            <p>Password</p>
+            <input type="text" name="password" className="password"></input>
+            <p>Forgot Passowrd?</p>
+
+            <button type="submit" className="login" onClick={this.handleRoute}>Log In</button>
+            <button type="submit" className="signup">Sign Up </button>
+
+        </div>
+    )
+}
+
+export default LogIn;
